f5b6143a0f176585fcd781edd3ddc6e5a868dfb0
phuocantd <phuocantd@gmail.com>
2024-05-05 00:30:22 +0700

    Upgrade version 0.74.1

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  overrides: [
+    {
+      plugins: [
+        [
+          '@babel/plugin-transform-private-methods',
+          {
+            loose: true,
+          },
+        ],
+      ],
+    },
+  ],
   plugins: [
     [
       'module-resolver',
