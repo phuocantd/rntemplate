33b3a471c333127e9cf71602389a77bb6e321759
an.nguyen <phuocantd@gmail.com>
2026-02-17 22:21:46 +0700

    feat: migrate language initialization from useConfigApp to useAppBootstrap

--- template/src/hooks/useAppBootstrap.ts
+++ template/src/hooks/useAppBootstrap.ts
@@ -1,5 +1,9 @@
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
+import { useDeviceContext } from 'twrnc';
+import { StorageEnum } from '~configs/constants';
+import tw from '~configs/tw';
 import {
   selectAuthRefreshToken,
   selectAuthToken,
@@ -8,11 +12,29 @@ import { logout, updateFields } from '~features/auth/auth.slice';
 import { authService } from '~services/api';
 import { configureAuthRefresh, setAuthTokens } from '~services/axios.service';
 import type { AppDispatch } from '~store';
+import { mmkv } from '~store/mmkvStorage';
 
 export const useAppBootstrap = () => {
+  const { i18n } = useTranslation();
   const dispatch = useDispatch<AppDispatch>();
   const token = useSelector(selectAuthToken);
   const refreshToken = useSelector(selectAuthRefreshToken);
+  useDeviceContext(tw);
+
+  useEffect(() => {
+    const initLanguage = async () => {
+      try {
+        const language = mmkv.getString(StorageEnum.LANGUAGE);
+        if (language) {
+          await i18n.changeLanguage(language);
+        }
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+
+    initLanguage();
+  }, [i18n]);
 
   useEffect(() => {
     configureAuthRefresh({
